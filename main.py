a79132aab3e93512638d059c9f4161e28284eb8d
parastoo naderi <roniaania@gmail.com>
2024-11-14 03:08:13 +0330

    The buttons for Rock, Paper, and Scissors have been set in the form, and the functions related to the commands have been defined.

--- main.py
+++ main.py
@@ -4,3 +4,29 @@ import random
 root = CTk()
 
 root.geometry('400 x 400 + 900 +150')
+
+#def
+
+def Rock():
+    pass
+
+def scissors():
+    pass
+
+def paper():
+    pass
+
+
+# btn
+
+Rockbutton = CTkButton(root, text="Rock", width=50, command=Rock)
+Rockbutton.place(relx=0.03, rely=0.2)
+
+paperbutton = CTkButton(root, text="paper", width=50, command=paper)
+paperbutton.place(relx=0.35, rely=0.2)
+
+scissorsbutton = CTkButton(root, text="scissors", width=50, command=scissors)
+scissorsbutton.place(relx=0.67, rely=0.2)
+
+
+root.mainloop()
